8316725794ae56b6cfa9c4577e0c9867e6971438
Septemptus <sephiran@gmail.com>
2015-10-31 22:32:02 +0100

    Remove dist build, add src dir

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,5 +1,4 @@
 var gulp = require('gulp'),
-    merge = require('merge-stream'),
     rename = require('gulp-rename'),
     clean = require('gulp-clean'),
     minifycss = require('gulp-minify-css'),
@@ -35,68 +34,56 @@ gulp.task('clean-rel', function () {
 });
 
 gulp.task('copy-lib', ['clean-build'], function () {
-    return gulp.src('lib/*')
-        .pipe(gulp.dest('build/dist/lib'))
-        .pipe(gulp.dest('build/dev/lib'));
+    return gulp.src('src/lib/*')
+        .pipe(gulp.dest('build/lib'))
 });
 
 gulp.task('styles', ['clean-build'], function () {
-    var minifyStream = gulp.src('styles/*.css')
+    return gulp.src('src/styles/*.css')
         .pipe(minifycss())
         .pipe(rename({basename: 'styles', suffix: '.min'}))
-        .pipe(gulp.dest('build/dist'));
-
-    var devStream = gulp.src('styles/*.css')
-        .pipe(gulp.dest('build/dev/styles'));
-
-    return merge(minifyStream, devStream);
+        .pipe(gulp.dest('build'));
 });
 
 gulp.task('minify', ['clean-build'], function () {
-    return gulp.src(['./*.js', '!./Gulpfile.js', '!popup.js'])
+    return gulp.src(['src/*.js', '!src/popup.js'])
         .pipe(uglify())
-        .pipe(concat('olx-relister.js'))
-        .pipe(gulp.dest('build/dist'));
-});
-
-gulp.task('copy-dev', ['clean-build'], function () {
-    return gulp.src(['./*.js', '!./Gulpfile.js', 'manifest.json', './*html'])
-        .pipe(gulp.dest('build/dev'));
+        .pipe(concat('olx-relister.min.js'))
+        .pipe(gulp.dest('build'));
 });
 
 gulp.task('minify-popup-script', ['clean-build'], function () {
-    return gulp.src('./popup.js')
+    return gulp.src('src/popup.js')
         .pipe(uglify())
-        .pipe(gulp.dest('build/dist'));
+        .pipe(gulp.dest('build'));
 });
 
 gulp.task('modify-manifest', ['clean-build'], function () {
-    return gulp.src('manifest.json')
+    return gulp.src('src/manifest.json')
         .pipe(jsonedit(function (json) {
-            json.background.scripts = ["lib/moment.min.js", "lib/q.min.js", "lib/gapi.js", "olx-relister.js"];
+            json.background.scripts = ["lib/moment.min.js", "lib/q.min.js", "lib/gapi.js", "olx-relister.min.js"];
             return json;
         }))
-        .pipe(gulp.dest('build/dist'));
+        .pipe(gulp.dest('build'));
 });
 
 gulp.task('copy-html', ['clean-build'], function () {
-    return gulp.src('./*.html')
-        .pipe(htmlreplace({js: ['olx-relister.js', 'popup.js'], css: 'styles.min.css'}))
-        .pipe(gulp.dest('build/dist'));
+    return gulp.src('src/*.html')
+        .pipe(htmlreplace({js: ['olx-relister.min.js', 'popup.js'], css: 'styles.min.css'}))
+        .pipe(gulp.dest('build'));
 });
 
 gulp.task('copy-images', ['clean-build'], function () {
-    return gulp.src('img/*')
-        .pipe(gulp.dest('build/dist/img'))
-        .pipe(gulp.dest('build/dev/img'));
+    return gulp.src('src/img/*')
+        .pipe(gulp.dest('build/img'));
 });
 
 gulp.task('copy-assets', ['copy-html', 'copy-images', 'clean-build']);
 
-gulp.task('build', ['copy-lib', 'copy-dev', 'copy-assets', 'modify-manifest', 'styles', 'minify', 'minify-popup-script']);
+gulp.task('build', ['copy-lib', 'copy-assets', 'modify-manifest', 'styles', 'minify', 'minify-popup-script']);
 
 gulp.task('release', ['clean-rel', 'build', 'bump-versions'], function () {
-    return gulp.src('build/dist/**')
+    return gulp.src('build/**')
         .pipe(zip('olx-relister.zip'))
         .pipe(gulp.dest('rel'));
 });
@@ -108,17 +95,17 @@ gulp.task('bump-package-ver', ['clean-rel', 'build'], function () {
 });
 
 gulp.task('bump-manifest-ver', ['clean-rel', 'build'], function () {
-    return gulp.src('./manifest.json')
+    return gulp.src('./src/manifest.json')
         .pipe(bump({type: getBumpType()}))
-        .pipe(gulp.dest('.'));
+        .pipe(gulp.dest('src'));
 });
 
 gulp.task('bump-dist-manifest-ver', ['clean-rel', 'build'], function () {
-    return gulp.src('dist/manifest.json')
+    return gulp.src('build/manifest.json')
         .pipe(bump({type: getBumpType()}))
-        .pipe(gulp.dest('dist'));
+        .pipe(gulp.dest('build'));
 });
 
-gulp.task('bump-versions', ['bump-package-ver', 'bump-manifest-ver', 'bump-dist-manifest-ver']);
+gulp.task('bump-versions', ['build', 'bump-package-ver', 'bump-manifest-ver', 'bump-dist-manifest-ver']);
 
 gulp.task('default', ['build']);
